f37627dc6ce024945651993156a88a01c98bc7e9
datnguyen-htg <dat.nguyen@hometogo.com>
2023-10-17 00:13:29 +0200

    add curve_fitting

---
+++ utils/curve_fitting.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from numba import njit
+
+@njit(nogil=True, parallel=True, cache=True)
+def linear(x, a, b):
+    return a + x*b
+
+@njit(nogil=True, parallel=True, cache=True, forceobj=True)
+def diminishing(x, a, b, c):
+    return a + b*x**c / c
+
+@njit(nogil=True, parallel=True, cache=True)
+def gauss(x, a, x0, sigma):
+    return a * np.exp(-(x - x0)**2 / (2 * sigma**2))
+
+@njit(nogil=True, parallel=True, cache=True)
+def sigmoid(x, A, k, x0):
+    return A/(1+np.exp(-k*(x-x0)))
