b322d97d63cbfada2195eec5cc7de64fc0a5d77a
datnguyen-htg <dat.nguyen@hometogo.com>
2024-06-11 00:08:47 +0200

    update charts

--- test_e3887_evaluation/sql_template/appsflyer_install.sql
+++ test_e3887_evaluation/sql_template/appsflyer_install.sql
@@ -1,8 +1,14 @@
 select date_trunc(day, install_time) dt,
-       count(distinct app_user_id) as installs
+        count(distinct
+            case
+            when campaign_name in ('web_app_install_large_overlay_deals', 'web_app_install_large_overlay_deals_dach')
+                then app_user_id
+            else null end
+            ) as overlay_installs,
+       count(distinct app_user_id) as total_installs
 from stage.reporting_marketing.appsflyer_installs_uninstalls
 where
     activity_kind = 'install'
-    and install_time > '2023-12-01'
+    and install_time > '2023-10-01'
 group by 1
 order by 1;
--- test_e3887_evaluation/sql_template/revenue.sql
+++ test_e3887_evaluation/sql_template/revenue.sql
@@ -3,7 +3,11 @@ select
     date_trunc('day', SESSION_TIMESTAMP) dt,
 	ID,
 	count(distinct cookie_user_id) users,
+    count(distinct iff(visitor_group='Returning Visitor', cookie_user_id,null)) returning_users,
+    returning_users/users as returning_users_share,
 	count(distinct session_id) sessions,
+    count(distinct iff(visitor_group='Returning Visitor', session_id,null)) returning_sessions,
+    returning_sessions/sessions as returning_sessions_share,
 	sum(revenue) revenues,
     sum(booking_quantity_direct_before_cancellations_onsite_booking) onsite_bookings,
     sum(searches) searches,
@@ -14,8 +18,10 @@ select
     sum(onsite_bookings) over(partition by ID order by DT asc) cum_onsite_bookings,
     sum(UWOB) over(partition by ID order by DT asc) cum_UWOB,
 
+
     cum_revenues/cum_users RPU,
     cum_onsite_bookings/cum_users onsite_booking_per_user
+    
 from STAGE.DERIVED_AB_TESTS.AB_TESTS_USERS_BASE_SESSIONS_W_MARKERS
 where TEST_ID = 'e3887'
 and SESSION_TIMESTAMP >= '2024-01-01'
