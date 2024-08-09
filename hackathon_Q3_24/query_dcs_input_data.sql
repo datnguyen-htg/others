552dce7160ef2ba949883223dae9e9a16a742653
datnguyen-htg <dat.nguyen@hometogo.com>
2024-08-09 16:17:45 +0200

    update

--- hackathon_Q3_24/query_dcs_input_data.sql
+++ hackathon_Q3_24/query_dcs_input_data.sql
@@ -2,6 +2,3 @@ select *
 from stage.reporting_finance.dcs_input_data
 where true
 and site_global_region = 'DACH'
-and site_id= 'hometogo.de'
-and INTER_COMPANY_GROUP = 'FALSE'
-and DEAL_TYPE ilike '%onsite%'
